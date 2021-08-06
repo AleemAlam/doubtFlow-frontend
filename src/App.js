5baaf026b59c7d9a921dad37e07132c1baa9184d
Nilesh Kokate <nilp7744@gmail.com>
2021-08-07 05:27:11 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
 import { Login } from './Components/LogIn/Login';
 import Navbar from './Components/Navbar';
-import { RegisterMain } from './Components/Regis/RegisterMain';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-     <RegisterMain />
+      <Login/>
    
      
     </div>
