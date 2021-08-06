9bb44795e6f03cb7a9f1743d46cea029a40bc059
Nilesh Kokate <nilp7744@gmail.com>
2021-08-07 03:47:07 +0530

    changes

--- src/Components/Regis/RegisterMain.jsx
+++ src/Components/Regis/RegisterMain.jsx
@@ -1,10 +1,62 @@
-import { Button, TextField } from "@material-ui/core"
-import React from "react"
+import { Button, TextField} from "@material-ui/core"
+import React,{useState } from "react"
 import "../../Styles/Register.css"
 import ComputerIcon from '@material-ui/icons/Computer';
 
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 const RegisterMain = ()=>{
+
+    const obj1 = {
+        name2: "",
+        number: "",
+        email: "",
+        password: "",
+       
+      };
+      const [query, setQuery] = useState(obj1);
+      const {name2,number,email,password} = query;
+
+      const handleChange= (e) => {
+        const { name, value} = e.target;
+    
+
+        console.log(name)
+        console.log(name,value,e)
+      
+        setQuery({
+          ...query,
+          [name]: value,
+        });
+      };
+
+      const handleRegister = () => {
+
+        console.log(query)
+        if (query.name2 !== "" && query.number !== "" && query.email !== "" && query.password !== "") {
+            let payload = {
+              name:query.name2 ,
+              email: query.email ,
+              number:query.number,
+              password: query.password ,
+             
+            };
+            setQuery(payload);
+            // dispatch(loginruser(payload));
+          } else {
+            if (query.name2  === "") {
+              alert("Please Enter Full Name");
+            } else if (query.email === "") {
+              alert("Please Enter Your Email");
+            }
+            else if (query.password === "") {
+                alert("Please Enter Your Password");
+              }
+              else if ( query.stay=== "") {
+                alert("Please Enter Your Email");
+            }
+          }
+
+      }
     return(
 
         <div>
@@ -14,7 +66,7 @@ const RegisterMain = ()=>{
                 <div className="Head-subdiv-register">
 
                     <h2>Join the club</h2>
-                    
+                    2,number2,email,password
 
                     <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus.
@@ -37,20 +89,39 @@ const RegisterMain = ()=>{
 
                     <h1 style={{color:"black"}}>Welcome</h1>
 
-                <TextField id="outlined-search" label="Full Name" type="text" variant="outlined" size="small"/>
+                <TextField 
+        
+                label="Full Name" 
+                type="search"
+                variant="outlined"
+                size="small"
+                value={name2}
+                name="name2"   onChange={handleChange} />
+              
                 <br/>     <br/>
 
                     
-                <TextField id="outlined-search" label="Phone No" type="Number" variant="outlined" size="small" />
+                <TextField  onChange={handleChange} label="Phone No" type="Number" variant="outlined" size="small" 
+                value={number}
+                name="number" 
+           
+                onChange={handleChange} required/>
+          
                 <br/>     <br/>
                     
-                <TextField id="outlined-search" label="Email" type="email" variant="outlined" size="small" />
+                <TextField onChange={handleChange} label="Email" type="email" variant="outlined" size="small" required  
+                 value={email}
+                name="email"/>
+                 onChange={handleChange}
                 <br/>     <br/>
-                <TextField id="outlined-search" label="Password" type="password" variant="outlined" size="small" />
+                <TextField  onChange={handleChange} label="Password" type="password" variant="outlined" size="small"
+                value={password}
+                name="password"/>
+                onChange={handleChange}
                 <br/>     <br/>
                     
               
-                <Button variant="contained" color="primary">REGISTER</Button>
+                <Button variant="contained" color="primary" onClick={handleRegister }>REGISTER</Button>
 
                 </div>
 
